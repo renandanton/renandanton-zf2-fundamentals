5b3e404e98a9e307c9e6074460156aa30a0745b1
Renan Danton <renandanton@yahoo.com.br>
2016-07-17 16:02:26 -0300

    Modulo 7 Entendendo rotas Exercicio 1

--- module/Market/config/module.config.php
+++ module/Market/config/module.config.php
@@ -3,33 +3,77 @@
 return array(
     'router' => array(
         'routes' => array(
-            'market' => array(
-                'type' => 'Literal',
-                'options' => array(
-                    'route' => '/market',
-                    'defaults' => array(
-                        'controller' => 'market-index-controller',
-                        'action' => 'index',
-                    ),
-                ),
-                'may_terminate' => true,
-                'child_routes' => array(
-                    'default' => array(
-                        'type' => 'Segment',
+              'home' => array(
+                  'type' => 'Literal',
+                  'options' => array(
+                      'route' => '/',
+                      'defaults' => array(
+                          'controller' => 'market-index-controller',
+                          'action' => 'index',
+                      ),
+                  ),
+              ),
+              'market' => array(
+                  'type' => 'Literal',
+                  'options' => array(
+                      'route' => '/market',
+                      'defaults' => array(
+                          'controller' => 'market-index-controller',
+                          'action' => 'index',
+                      ),
+                  ),
+
+               'may_terminate' => true,
+               'child_routes' => array(
+                    'view' => array(
+                        'type' => 'Literal',
                         'options' => array(
-                            'route' => '/[:controller[/:action]]',
-                            'constraints' => array(
-                                'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
-                                'action'     => '[a-zA-Z][a-zA-Z0-9_-]*',
-                             ),
-                             'defaults' => array(
+                            'route' => '/view',
+                            'defaults' => array(
+                                'controller' => 'market-view-controller',
+                                'action' => 'index',
                              ),
                         ),
+
+                    'may_terminate' => true,
+                    'child_routes' => array(
+                      'main' => array(
+                          'type' => 'Segment',
+                          'options' => array(
+                              'route' => '/main[/:category]',
+                              'defaults' => array(
+                                  'action' => 'index',
+                              ),
+                          ),
+                      ),
+                      'item' => array(
+                          'type' => 'Segment',
+                          'options' => array(
+                              'route' => '/item[/:itemId]',
+                              'defaults' => array(
+                                  'action' => 'item'
+                              ),
+                              'constraints' => array(
+                                  'itemId' => '[0-9]*',
+                              ),
+                          ),
+                      ),
                     ),
-                ),
-            ),
+                  ),
+                  'post' => array(
+                      'type' => 'Literal',
+                      'options' => array(
+                          'route' => '/post',
+                           'defaults' => array(
+                               'controller' => 'market-post-controller',
+                               'action' => 'index',
+                           ),
+                       ),
+                  ),
+              ),
+           ),
         ),
-      ),
+     ),
 
     'controllers' => array(
         'invokables' => array(
--- module/Market/src/Market/controller/ViewController.php
+++ module/Market/src/Market/controller/ViewController.php
@@ -9,14 +9,14 @@ class ViewController extends AbstractActionController
 {
     public function indexAction()
     {
-      $category = $this->params()->fromQuery('category');
+      $category = $this->params()->fromRoute('category');
 
       return new ViewModel(["category" => $category]);
     }
 
     public function itemAction()
     {
-        $itemId = $this->params()->fromQuery('itemId');
+        $itemId = $this->params()->fromRoute('itemId');
 
         if(!$itemId) {
             $this->flashmessenger()->addMessage('Item not found');
